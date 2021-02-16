d9fbcbe553d19c2d1a1a0fd7f35a3d1e00c6e026
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-02-16 20:25:50 +0100

    triangle

--- canvas.js
+++ canvas.js
@@ -6,14 +6,21 @@ window.addEventListener(`load`, () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 5;
-  ctx.strokeRect(100, 100, 200, 200);
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 10;
-  ctx.strokeRect(200, 200, 200, 200);
-  ctx.fillStyle = "purple";
-  ctx.fillRect(300, 300, 200, 200);
+  //   ctx.strokeStyle = "red";
+  //   ctx.lineWidth = 5;
+  //   ctx.strokeRect(100, 100, 200, 200);
+  //   ctx.strokeStyle = "blue";
+  //   ctx.lineWidth = 10;
+  //   ctx.strokeRect(200, 200, 200, 200);
+  //   ctx.fillStyle = "purple";
+  //   ctx.fillRect(300, 300, 200, 200);
+
+  ctx.beginPath();
+  ctx.moveTo(100, 100);
+  ctx.lineTo(200, 100);
+  ctx.lineTo(200, 150);
+  ctx.closePath();
+  ctx.stroke();
 });
 
 // Resizing
