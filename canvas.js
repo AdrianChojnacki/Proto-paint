f6d4e694205de472e3d05d40847c6ccd627e515b
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-02-16 20:36:10 +0100

    We can draw now

--- canvas.js
+++ canvas.js
@@ -2,7 +2,7 @@ window.addEventListener(`load`, () => {
   const canvas = document.querySelector(`#canvas`);
   const ctx = canvas.getContext("2d");
 
-  // Set size
+  // Set canvas size
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 
@@ -15,15 +15,52 @@ window.addEventListener(`load`, () => {
   //   ctx.fillStyle = "purple";
   //   ctx.fillRect(300, 300, 200, 200);
 
-  ctx.beginPath();
-  ctx.moveTo(100, 100);
-  ctx.lineTo(200, 100);
-  ctx.lineTo(200, 150);
-  ctx.closePath();
-  ctx.stroke();
+  //   ctx.beginPath();
+  //   ctx.moveTo(100, 100);
+  //   ctx.lineTo(200, 100);
+  //   ctx.lineTo(200, 150);
+  //   ctx.closePath();
+  //   ctx.stroke();
+
+  // Interface
+  const lineWidth = 10;
+  const lineCap = "round";
+  const strokeStyle = "black";
+
+  // Flag
+  let painting = false;
+
+  // Flag switch functions
+  const startPosition = (e) => {
+    painting = true;
+    draw(e);
+  };
+  const finishedPosition = () => {
+    painting = false;
+    ctx.beginPath();
+  };
+
+  // Draw function
+  const draw = (e) => {
+    if (!painting) return;
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = lineCap;
+    ctx.strokeStyle = strokeStyle;
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+  };
+
+  // Listeners
+  canvas.addEventListener(`mousedown`, startPosition);
+  canvas.addEventListener(`mouseup`, finishedPosition);
+  canvas.addEventListener(`mousemove`, draw);
 });
 
-// Resizing
+// Keep canvas size on window resizing
 window.addEventListener(`resize`, () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
